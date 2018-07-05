6456eedc4649f6da31e8978e0f43613a55651998
raphsutti <neoster9@gmail.com>
2018-07-06 07:00:03 +1000

    testing react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends Component {
+  render() { return <div>Goodbye!</div> }
+}
 
 class App extends Component {
   render() {
@@ -11,7 +20,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <BrowserRouter>
+          <div>
+            <Route path="/hello" component={Hello} />
+            <Route path="/goodbye" component={Goodbye} />
+          </div>
+        </BrowserRouter>
         </p>
       </div>
     );
